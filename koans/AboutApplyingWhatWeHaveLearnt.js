dd01a3233d14fe914161cc52fd9d6fb72ef00bc5
Josephine <bluejello@gmail.com>
2016-07-01 13:14:33 -0700

    Fix all error in AboutApplyingWhatWeHaveLearnt.js
    Utilized understanding of everything we have learned so far
    from Koans to solve each problem.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,15 +30,18 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
-
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      productsICanEat = _(products).filter(function(product){ 
+        return !(product.containsNuts || _(product.ingredients).any(function(ingredient){ return ingredient === "mushrooms"; })); 
+      });
+      
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -52,13 +55,21 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = _(_.range(1,1000)).chain()
+                                .reduce(function(sum, num){
+                                  if (num % 3 === 0 || num % 5 === 0)
+                                    sum += num;
+                                  return sum;
+                                },0)
+                                .value();    
 
-    expect(233168).toBe(FILL_ME_IN);
+    /* try chaining range() and reduce() */
+
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -71,39 +82,134 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+    ingredientCount = _(products).chain()
+               .map(function(product){ return product.ingredients; })
+               .flatten()
+               .reduce(function(ingredientCount, ingredient){
+                  if (ingredient in ingredientCount){
+                    ingredientCount[ingredient] += 1;
+                  }else{
+                    ingredientCount[ingredient] = 1;
+                  }
+                  return ingredientCount;
+               },{})
+               .value();
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR ADVANCED */
-  /*
-  it("should find the largest prime factor of a composite number", function () {
   
+  it("should find the largest prime factor of a composite number", function () {
+    function isPrime(num){
+      var prime = true;
+      for (var i=2; i<num; i++){
+        if (num % i === 0)
+          prime = false;
+      }
+      return prime;
+    }
+    function largestPrimeFactor(num){
+      if (isPrime(num)) 
+        return "please use a composite number"; 
+      else{
+        for (var i=num-1; i>1; i--){
+          if (num % i === 0 && isPrime(i)){
+            return i;
+          }
+        }
+      }
+      return "not found";
+    }
+    
+    expect(largestPrimeFactor(77)).toBe(11);
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
-    
+    function isPalindrome(num){
+      var numArray = String(num).split("");
+      for(var i=0,j=numArray.length-1; i<numArray.length/2; i++, j--){
+        if (numArray[i] !== numArray[j]) return false;
+      }
+      return true;
+    }
+
+    var pal = "no palindrome found";
+    for(var i=999*999; i>100*100; i--){ //tests range from largest product of two 3 digit numbers to smallest
+      if (isPalindrome(i)){
+        pal = i;
+        break;
+      }
+    }
+
+    expect(pal).toBe(997799);
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
-      
-    
+    function divisibleByNumsThrough20(num){
+      var divisible = true;
+      for (var i=1; i<=20; i++){
+        if (num % i !== 0){
+          divisible = false;
+        }
+      }
+      return divisible;
+    }
+
+    var smallestNum = 20;
+    while (!divisibleByNumsThrough20(smallestNum)){
+      smallestNum++;
+    }
+
+    expect(smallestNum).toBe(232792560);
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
-    
+    function diffSumOfSqsAndSqOfSums(){ //can take any number of numbers to compute
+      var sumOfSqs = 0, sum = 0;
+      for (var i=0; i<arguments.length; i++){
+        sumOfSqs += arguments[i] * arguments[i];
+        sum += arguments[i];
+      }
+      return Math.max(sumOfSqs - sum * sum, sum * sum - sumOfSqs); //return positive difference
+    }
+
+    expect(diffSumOfSqsAndSqOfSums(1,2,3,4)).toBe(70);
   });
 
   it("should find the 10001st prime", function () {
+    function isPrime(num){
+      var prime = true;
+      for (var i=2; i<num; i++){
+        if (num % i === 0)
+          prime = false;
+      }
+      return prime;
+    }
 
+    function findPrime(num){
+      if (num < 1) 
+        return "please enter a number greater than 0";
+      
+      var prime = 1, count = 0;
+      while (count !== num){
+        prime++;
+        if (isPrime(prime)){
+          count++;
+        }
+      }
+      return prime;
+    }
+
+    expect(findPrime(10001)).toBe(104743);
   });
-  */
+ 
 });
